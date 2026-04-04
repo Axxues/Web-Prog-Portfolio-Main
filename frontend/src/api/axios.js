0972d97ca1c1b0311c310f97a1be6836f7eb5a23
Axxues <nagatogeneta@gmail.com>
2026-04-05 00:49:29 +0800

    some updates

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
-export const API_ORIGIN = import.meta.env.VITE_API_ORIGIN || 'http://localhost:5000';
-
 const API = axios.create({
-  baseURL: `${API_ORIGIN}/api`,
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use((config) => {
