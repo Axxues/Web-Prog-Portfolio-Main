406550a93637ac310ff1356d65964917638080e5
Axxues <nagatogeneta@gmail.com>
2026-04-05 01:05:29 +0800

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-export const API_ORIGIN = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+export const API_ORIGIN = 'https://web-prog-portfolio-main-api.onrender.com';
 
 const API = axios.create({
   baseURL: `${API_ORIGIN}/api`,
